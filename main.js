cab35f6146d7cac8048d4b8e9d257e6e9fdecc26
Beatriz <beatrizmaecruz@proton.me>
2025-08-10 22:05:15 +1200

    Added indicator for learnt-languages to scroll + started attributions for assets

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("Funny seeing you here. I'm always in the console too. :D");
+console.log("Hey, funny seeing you here. I'm always in the console too. :D");
+
+const greet3 = document.getElementById('greet-3');
+const learntLanguagesHoverMsg = document.querySelector('#greet-3 .hover-message');
+
+let languages_hasHovered = false; 
+ 
+// Hover messages functions
+greet3.addEventListener('mouseover', () => {
+    learntLanguagesHoverMsg.show();
+    learntLanguagesHoverMsg.style.animation = 'fadeInOpacity 1s 1';
+});
+
+greet3.addEventListener('mouseout', () => {
+    learntLanguagesHoverMsg.close();
+});
